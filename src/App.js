c08daceb151c29091557289c7798849520e22776
bert <efarolcamposano@gmail.com>
2023-07-21 22:55:26 +0800

    update ui

--- src/App.js
+++ src/App.js
@@ -5,12 +5,8 @@ import ChatBox from "./components/ChatBox";
 import { Icon } from "@iconify/react";
 
 function App() {
-  const {
-    signInWithFacebook,
-    signInAsGuest,
-    user,
-    authChecked,
-  } = useContext(AuthContext);
+  const { signInWithFacebook, signInAsGuest, user, authChecked, loading } =
+    useContext(AuthContext);
 
   if (!authChecked) {
     return <div>Loading...</div>;
@@ -22,17 +18,24 @@ function App() {
 
   return (
     <div className="grid h-screen App place-items-center">
-      <div className="flex flex-col gap-2">
+      {loading ? (
+        <div className="absolute z-20 grid w-12 h-12 bg-white rounded shadow-lg place-content-center top-2/4 -translate-y-2/4">
+          <Icon icon="tabler:loader-2" className="w-5 h-5 animate-spin" />
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="flex flex-col gap-2 px-6 py-5 bg-white border rounded">
         <button
           onClick={signInWithFacebook}
-          className="flex items-center gap-2 px-4 py-2 text-white font-medium border rounded bg-[#3b5998]"
+          className="flex items-center gap-3 px-4 py-2 text-white font-medium border rounded bg-[#3b5998] hover:opacity-80"
         >
           <Icon icon="uil:facebook" className="w-5 h-5 border-none" />
           Sign in with Facebook
         </button>
         <button
           onClick={signInAsGuest}
-          className="flex items-center gap-2 px-4 py-2 font-medium border rounded bg-[#f4b400] text-white"
+          className="flex items-center gap-3 px-4 py-2 font-medium border rounded bg-[#f4b400] text-white hover:opacity-80"
         >
           <Icon icon="mdi:user-outline" className="w-5 h-5" />
           Continue as guest
--- src/context/AuthenticationContextProvider.js
+++ src/context/AuthenticationContextProvider.js
@@ -10,10 +10,11 @@ import { auth, provider } from "../firebase";
 export const AuthContext = createContext(null);
 
 const AuthenticationContextProvider = ({ children }) => {
-   const [user, setUser] = useState(null);
-   const [authChecked, setAuthChecked] = useState(false);
-   const [profilePic, setProfilePic] = useState(null);
-   const [displayName, setDisplayName] = useState(null);
+  const [user, setUser] = useState(null);
+  const [authChecked, setAuthChecked] = useState(false);
+  const [profilePic, setProfilePic] = useState(null);
+  const [displayName, setDisplayName] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -32,24 +33,29 @@ const AuthenticationContextProvider = ({ children }) => {
   });
 
   const signInWithFacebook = async () => {
+    setLoading(true);
     try {
       const result = await signInWithPopup(auth, provider);
-      const user = result.user;
+      // const user = result.user;
     } catch (error) {
       console.log("signInWithFacebook: ", error);
+    } finally {
+      setLoading(false);
     }
   };
 
   const signInAsGuest = async () => {
+    setLoading(true);
     try {
       const result = await signInAnonymously(auth);
       const user = result.user;
-      // console.log("user: ", user);
-      console.log("successfull signed in as guest");
+      console.log("user: ", result);
     } catch (error) {
       console.log("signInAsGuest: ", error);
+    } finally {
+      setLoading(false);
     }
-  }
+  };
 
   const handleSignOut = () => {
     signOut(auth)
@@ -65,6 +71,7 @@ const AuthenticationContextProvider = ({ children }) => {
     signInWithFacebook,
     signInAsGuest,
     handleSignOut,
+    loading,
     user,
     authChecked,
     profilePic,
